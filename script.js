6d453ff4083fe1fd5d117c68521d5da39b0985f1
Edsamn <eduardomsribas@gmail.comm>
2024-03-14 23:06:27 -0300

    paginação exemplo

---
+++ script.js
@@ -0,0 +1,55 @@
+const urlParams = new URLSearchParams(window.location.search)
+
+const limit = parseInt(urlParams.get("limit")) || 2;
+let offset = parseInt(urlParams.get("offset")) || 1;
+
+const div = document.getElementById("lista-dados");
+
+
+
+const fazerPaginacao = async () => {
+    try {
+
+        const data = {
+            limit,
+            offset
+        };
+
+        const response = await api.get("/massage", { params: data });
+
+        const mensagem = response.data.data;
+
+        mensagem.forEach(msg => {
+            const divCriada = document.createElement("div");
+            divCriada.innerHTML = `<p>${msg.id}</p>
+            <p>${msg.title}</p> 
+            <p>${msg.description}</p>`;
+
+            div.appendChild(divCriada);
+
+        });
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+fazerPaginacao()
+
+const proximaPagina = () => {
+
+    if (offset) {
+        offset += 1
+    }
+
+    div.innerHTML = ''
+    fazerPaginacao()
+}
+
+const paginaAnterior = () => {
+    if (offset > 1) {
+        offset -= 1
+    }
+    div.innerHTML = ''
+    fazerPaginacao()
+}
