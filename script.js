faf9ecdff8c5366ec8bf11cf49024e3dea5cb7bb
Edsamn <eduardomsribas@gmail.comm>
2024-03-18 15:34:48 -0300

    btn disabled added

--- script.js
+++ script.js
@@ -1,55 +1,53 @@
-const urlParams = new URLSearchParams(window.location.search)
+const urlParams = new URLSearchParams(window.location.search);
 
 const limit = parseInt(urlParams.get("limit")) || 2;
 let offset = parseInt(urlParams.get("offset")) || 1;
 
 const div = document.getElementById("lista-dados");
-
-
+const btnProximo = document.getElementById("proxima");
+const btnAnterior = document.getElementById("anterior");
 
 const fazerPaginacao = async () => {
-    try {
+  try {
+    const data = {
+      limit,
+      offset,
+    };
 
-        const data = {
-            limit,
-            offset
-        };
+    const response = await api.get("/massage", {params: data});
 
-        const response = await api.get("/massage", { params: data });
+    const mensagem = response.data.data;
 
-        const mensagem = response.data.data;
-
-        mensagem.forEach(msg => {
-            const divCriada = document.createElement("div");
-            divCriada.innerHTML = `<p>${msg.id}</p>
+    mensagem.forEach((msg) => {
+      const divCriada = document.createElement("div");
+      divCriada.innerHTML = `<p>${msg.id}</p>
             <p>${msg.title}</p> 
             <p>${msg.description}</p>`;
 
-            div.appendChild(divCriada);
+      div.appendChild(divCriada);
+    });
 
-        });
+    btnAnterior.disabled = offset === 1;
+    btnProximo.disabled = response.data.totalPages === offset;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-fazerPaginacao()
+fazerPaginacao();
 
 const proximaPagina = () => {
-
-    if (offset) {
-        offset += 1
-    }
-
-    div.innerHTML = ''
-    fazerPaginacao()
-}
+  if (offset) {
+    offset += 1;
+  }
+  div.innerHTML = "";
+  fazerPaginacao();
+};
 
 const paginaAnterior = () => {
-    if (offset > 1) {
-        offset -= 1
-    }
-    div.innerHTML = ''
-    fazerPaginacao()
-}
+  if (offset > 1) {
+    offset -= 1;
+  }
+  div.innerHTML = "";
+  fazerPaginacao();
+};
